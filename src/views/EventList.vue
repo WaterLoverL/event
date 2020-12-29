877b9c47916132c7b3f9e9523a970ac8750cc4af
chenyu <chenyul07581@outlook.com>
2020-12-29 19:39:35 +0800

    first commit

---
+++ src/views/EventList.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="events">
+    <h1>Events For Good</h1>
+    <event-card v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+import EventCard from "@/components/EventCard";
+
+export default {
+  name: 'Home',
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: [
+      {
+        id: 5928101,
+        category: 'animal welfare',
+        title: 'Cat Adoption Day',
+        description: 'Find your new feline friend at this event.',
+        location: 'Meow Town',
+        date: 'January 28, 2022',
+        time: '12:00',
+        petsAllowed: true,
+        organizer: 'Kat Laydee'
+      },
+        {
+          id: 524235234,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline friend at this event.',
+          location: 'Meow Town',
+          date: 'January 28, 2022',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee'
+        },
+        {
+          id: 53462357,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline friend at this event.',
+          location: 'Meow Town',
+          date: 'January 28, 2022',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee'
+        },
+        {
+          id: 4596894591,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline friend at this event.',
+          location: 'Meow Town',
+          date: 'January 28, 2022',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee'
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+</style>
