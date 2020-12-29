e58958686b33bf9f392a2f7972fdbcb1b169471b
chenyu <chenyul07581@outlook.com>
2020-12-29 19:43:23 +0800

    add dist

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+import EventServices from "@/services/EventService.js";
+
+export default {
+  props: ['id'],
+  data() {
+    return {
+      event: null,
+    }
+  },
+  created() {
+    EventServices.getEvent(this.id)
+    .then(response => {
+      this.event = response.data
+      console.log(response.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+}
+</script>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,68 +1,34 @@
 <template>
   <div class="events">
     <h1>Events For Good</h1>
-    <event-card v-for="event in events" :key="event.id" :event="event" />
+    <event-card
+        v-for="event in events"
+        :key="event.id"
+        :event="event"
+    />
   </div>
 </template>
 
 <script>
 import EventCard from "@/components/EventCard";
-
+import EventServices from "@/services/EventService";
 export default {
-  name: 'Home',
+  name: 'EventList',
   components: {
     EventCard
   },
   data() {
     return {
-      events: [
-      {
-        id: 5928101,
-        category: 'animal welfare',
-        title: 'Cat Adoption Day',
-        description: 'Find your new feline friend at this event.',
-        location: 'Meow Town',
-        date: 'January 28, 2022',
-        time: '12:00',
-        petsAllowed: true,
-        organizer: 'Kat Laydee'
-      },
-        {
-          id: 524235234,
-          category: 'animal welfare',
-          title: 'Cat Adoption Day',
-          description: 'Find your new feline friend at this event.',
-          location: 'Meow Town',
-          date: 'January 28, 2022',
-          time: '12:00',
-          petsAllowed: true,
-          organizer: 'Kat Laydee'
-        },
-        {
-          id: 53462357,
-          category: 'animal welfare',
-          title: 'Cat Adoption Day',
-          description: 'Find your new feline friend at this event.',
-          location: 'Meow Town',
-          date: 'January 28, 2022',
-          time: '12:00',
-          petsAllowed: true,
-          organizer: 'Kat Laydee'
-        },
-        {
-          id: 4596894591,
-          category: 'animal welfare',
-          title: 'Cat Adoption Day',
-          description: 'Find your new feline friend at this event.',
-          location: 'Meow Town',
-          date: 'January 28, 2022',
-          time: '12:00',
-          petsAllowed: true,
-          organizer: 'Kat Laydee'
-        },
-      ]
+      events: null
     }
-  }
+  },
+  created() {
+    EventServices.getEvents().then(response => {
+      this.events = response.data;
+    }).catch(error => {
+      console.log(error)
+    })
+  },
 }
 </script>
 
